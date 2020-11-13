e380be3f31846e97389874ee16d37895ab69d691
lautaro bringasq <lm.bringas98@gmail.com>
2020-11-13 00:13:54 -0300

    💣

--- test/test.sh
+++ test/test.sh
@@ -2,8 +2,4 @@
 
 set -ev
 
-docker run --rm \
-         -v /home/runner/work/catalogue-microservice/catalogue-microservice/go/src/github.com/sock-shop-demo/catalogue-microservice/vendor:/go/src/ \
-         -v /home/runner/work/catalogue-microservice/catalogue-microservice/go/src/github.com/sock-shop-demo/catalogue-microservice/test/..:/go/src/github.com/microservices-demo/catalogue \
-         -w /go/src/github.com/microservices-demo/catalogue \
-         -e GOPATH=/go/ golang:1.7 go test -v -covermode=count -coverprofile=coverage.out
+go test -v -covermode=count -coverprofile=coverage.out
