6b3bafb34612e5d5eaa1e2c783cc7c6cf48bc274
lautaro bringasq <lm.bringas98@gmail.com>
2020-11-12 23:38:36 -0300

    💥 testig ci

--- test/test.sh
+++ test/test.sh
@@ -2,33 +2,8 @@
 
 set -ev
 
-SCRIPT_DIR=`dirname "$0"`
-SCRIPT_NAME=`basename "$0"`
-SSH_OPTS=-oStrictHostKeyChecking=no
-
-if [[ "$(uname)" == "Darwin" ]]; then
-    DOCKER_CMD=docker
-else
-    DOCKER_CMD="sudo docker"
-fi
-
-if [[ -z $($DOCKER_CMD images | grep test-container) ]] ; then
-    echo "Building test container"
-    docker build -t test-container $SCRIPT_DIR > /dev/null
-fi
-
-echo "Testing $1"
-CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
-GOPATH=${PWD}/vendor
-$DOCKER_CMD run \
-    --rm \
-    --name test \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v $CODE_DIR:$CODE_DIR -w $CODE_DIR \
-    -e TRAVIS_JOB_ID=$TRAVIS_JOB_ID \
-    -e TRAVIS_BRANCH=$TRAVIS_BRANCH \
-    -e TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST \
-    -e TRAVIS=$TRAVIS \
-    -e GOPATH=$GOPATH \
-    test-container \
-    sh -c "export PYTHONPATH=\$PYTHONPATH:\$PWD/test ; python test/$@"
+docker run --rm \
+         -v /home/runner/work/catalogue-microservice/catalogue-microservice/go/src/github.com/sock-shop-demo/catalogue-microservice/vendor:/go/src/ \
+         -v /home/runner/work/catalogue-microservice/catalogue-microservice/go/src/github.com/sock-shop-demo/catalogue-microservice/test/..:/go/src/github.com/microservices-demo/catalogue \
+         -w /go/src/github.com/microservices-demo/catalogue \ 
+         -e GOPATH=/go/ golang:1.7 go test -v -covermode=count -coverprofile=coverage.out
