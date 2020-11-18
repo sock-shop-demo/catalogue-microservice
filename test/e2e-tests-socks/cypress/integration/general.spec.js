2f43b27f1bee591cafbc071526dc0f2c03c55e4a
lautaro bringasq <lm.bringas98@gmail.com>
2020-11-18 14:39:13 -0300

    🥺 integration test ?

---
+++ test/e2e-tests-socks/cypress/integration/general.spec.js
@@ -0,0 +1,22 @@
+import { baseURL } from '../../../cypress';
+
+describe('API Testing with Cypress', () => {
+
+   beforeEach(() => {
+       cy.request(`${baseURL}/catalogue`).as('catalogue');
+   });
+
+   it('Validate the header', () => {
+       cy.get('@pikachu')
+           .its('headers')
+           .its('content-type')
+           .should('include', 'application/json; charset=utf-8');
+   });
+
+   it('Validate the status code', () => {
+       cy.get('@catalogue')
+           .its('catalogue')
+           .should('equal', 200);
+   });
+  
+});
